0c90789199582288d3cd5ff434536720e26ddb9d
clong desktop <chip_long@yahoo.com>
2022-05-01 00:02:54 -0600

    polished the id pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
+
   return {
     paths,
     fallback: false,
@@ -22,11 +26,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {console.log(postData)}
-      {postData.title} <br />
-      {postData.id} <br />
-      {postData.date}
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
